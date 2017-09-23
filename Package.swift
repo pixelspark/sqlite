e4e889123447390620824a6fda52e035802e3bde
Tommy van der Vorst <tommy@pixelspark.nl>
2017-09-23 13:59:00 +0200

    Upgrade to Swift 4

--- Package.swift
+++ Package.swift
@@ -1,10 +1,15 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "SQLite",
-    targets: [
-        Target(name: "SQLite", dependencies: ["CSQLite"])
-    ],
+    products: [
+		.library(name: "SQLite", targets: ["SQLite"]),
+	],
     dependencies: [
+	],
+    targets: [
+		.target(name: "CSQLite", dependencies: []),
+        .target(name: "SQLite", dependencies: ["CSQLite"])
     ]
 )
